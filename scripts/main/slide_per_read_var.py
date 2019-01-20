eb1a64a38e8c8bc330ad22e0188dafddf37319b8
UANLEE <elzedliu@gmail.com>
2019-01-20 16:39:48 +0100

    Add files via upload

--- scripts/main/slide_per_read_var.py
+++ scripts/main/slide_per_read_var.py
@@ -2,11 +2,21 @@ import sys
 from collections import defaultdict
 import pandas as pd
 from collections import deque
+import gzip
+import os
 
 ''''
-different from version1
-this version, aka, version2 combines deletions in reads
-so as to print out complete 5mers instead of gaps in 5mers
+input file:
+(summed up from sam2tsv results using 
+#REF,REF_POS,REF_BASE,READ_NAME,READ_POSITION,READ_BASE,BASE_QUALITY,MISMATCH,INSERTION,DELETION
+cc6m_2244_T7_ecorv,31,A,4ea19788-8597-42c4-a7c2-3de4da5a7390,38,A,16,0,0,0
+cc6m_2244_T7_ecorv,32,T,4ea19788-8597-42c4-a7c2-3de4da5a7390,39,T,3,0,0,0
+
+this will output one base one line format 
+and
+one kmer one line format
+
+
 '''
 
 def window (seq,size=5):
@@ -19,6 +29,7 @@ def window (seq,size=5):
         yield win
 
 # main()
+relative_pos = ['-2','-1','0','+1','+2']
 rd_rf_mem_di = [] #defaultdict(list)
 rd_bases = [] #defaultdict(str)
 rf_bases = [] #defaultdict(str)
@@ -30,20 +41,28 @@ ins = [] #defaultdict(str)
 delt = [] #defaultdict(str)
 
 #REF", 'REF_POS','REF_BASE','READ_NAME','READ_POSITION','READ_BASE','BASE_QUALITY','MISMATCH','INSERTION','DELETION'
-fh = open (sys.argv[1],'r')
+prefix = ''
+if sys.argv[1].endswith(".gz"):
+	fh = gzip.open(sys.argv[1],'r')
+	prefix = sys.argv[1].replace('.gz','')
+else:
+	fh = open (sys.argv[1],'r')
+	prefix = sys.argv[1]
 h = fh.readline()
 row_counter = 1
 
-out_header = ",".join(['#ReadKmer','RefKmer','Ref','RefPos',
-                       'Read','ReadPos','Q','M','I','D'])
-print out_header
+slided_file = prefix+'.slided.onebase.oneline'
+slided_fh = open (slided_file,'w')
+slided_header = '#Relative_Pos,window,ReadKmer,RefKmer,Ref,RefPos,RefBase,Read,ReadPos,ReadBase,Q,M,I,D'
+print >>slided_fh, slided_header
 
-# first line always starts with a read containing perfect match or variant other than indel;
+# first line always starts with a read containing perfect match or variant other than deletion;
+## slided var
 firstLine = fh.readline()
 tmp = firstLine.strip().split(',')
 rd = tmp[3]
 ref = tmp[0]
-comb = rd+'\t'+ref
+comb = rd+' '+ref
 rd_rf_mem_di.append (comb)
 rd_bases.append (tmp[5])
 rf_bases.append (tmp[2])
@@ -58,12 +77,15 @@ for line in fh:
     if line.startswith('#'):
         continue
     tmp = line.strip().split(',')
+    if len (tmp) < 10:
+	continue
     rd = tmp[3]
     ref = tmp[0]
-    comb = rd+'\t'+ref
+    comb = rd+' '+ref
+    
     if comb in rd_rf_mem_di:
-        if int (tmp[9]) == 0 :
-            rd_rf_mem_di.append(comb)
+	if int (tmp[9]) == 0 :
+	    rd_rf_mem_di.append (comb) 
             rd_bases.append (tmp[5])
             rf_bases.append (tmp[2])
             rd_pos.append (tmp[4])
@@ -73,30 +95,36 @@ for line in fh:
             ins.append(tmp[8])
             delt.append(tmp[9])
         elif int(tmp[9]) > 0:
-            delt[-1] = str ( int (delt[-1]) + 1)
-            rf_bases[-1] = rf_bases[-1] +'+'+tmp[2]
-            #qual.append(tmp[6])
-            #rd_rf_mem_di.append (comb)
-            rf_pos[-1] += ';'+tmp[1]
-            #rd_pos[-1] += ';'+rd_pos[-1]
-            rd_bases[-1] += '-' + tmp[2]
-            #qual[-1] += '-' + qual[-1]
+            delt[-1] = str(int(delt[-1]) + 1)
+            rf_bases[-1] += '+' + tmp[2]
+            rf_pos[-1] += ':' + tmp[1]    	
     else:
         for i in window(range(len(rd_rf_mem_di)), 5):
-            try:
-                last = i[-1] + 1
-            except:
-                sys.stderr.write ('exception',i, rd_rf_mem_di[i], rd_pos[i],rf_pos[i])
+	    last = None
+	    rd_win_pos = '' 		    
+	    if i[-1] is None:  # when a read have less than window size number of positions mapped to a reference
+		last = i[0] + len (i) + 1  
+		rd_win_pos = ':'.join(rd_pos[i[0]:last])
+	    else:
+		last = i[-1] + 1
+		rd_win_pos = ':'.join(rd_pos[i[0]:last])
+		#sys.stderr.write ('exception',i, rd_rf_mem_di[i], rd_pos[i],rf_pos[i])
+                #sys.stderr.write ('exception',i, rd_rf_mem_di[i], rd_pos[i],rf_pos[i])
             RD, REF =  rd_rf_mem_di[-1].split()
             rd_kmer = ''.join (rd_bases[i[0]:last])
             rf_kmer = ''.join (rf_bases[i[0]:last])
-	    rf_positions = '|'.join (rf_pos[i[0]:last])
-	    rd_positions = '|'.join (rd_pos[i[0]:last])
-   	    qualities = '|'.join (qual[i[0]:last])
-	    mismatches = '|'.join (mis[i[0]:last])
-  	    insertions = '|'.join (ins[i[0]:last])
-            deletions = '|'.join (delt[i[0]:last])
-	    print ",".join ([rd_kmer, rf_kmer,REF,rf_positions,RD,rd_positions,qualities,mismatches,insertions,deletions])
+	    int_ele = 0	
+            for k in (i):
+		if isinstance (k,int):
+			#sys.stderr.write (str (len (rd_rf_mem_di)) + ' rf_rd: ' + rd_rf_mem_di[0] + ' ' + rd_rf_mem_di[-1] + ' '+ ':'.join (map (str, rd_pos)) + ' ' + ':'.join (map (str, i))+'\n' )
+                	print >>slided_fh,  ",".join ([relative_pos[int_ele], rd_win_pos, rd_kmer, rf_kmer, REF, rf_pos[k],rf_bases[k],
+                                 RD,rd_pos[k],rd_bases[k],
+                                 qual[k],mis[k],ins[k],delt[k]])
+			int_ele += 1
+		elif k is None: # less than 5 sites of a read mapped
+			print >>slided_fh,  ",".join ([relative_pos[int_ele], rd_win_pos, rd_kmer, rf_kmer, REF, 'Nan,Nan',
+                                RD,'Nan,Nan,Nan,Nan,Nan,Nan'])
+			int_ele += 1
         rd_rf_mem_di = [] #defaultdict(list)
         rd_bases = [] #defaultdict(str)
         rf_bases = [] #defaultdict(str)
@@ -117,12 +145,150 @@ for line in fh:
         delt.append(tmp[9])
 
 for i in window(range(len(rd_rf_mem_di)),5):
-    last = len (i)
-    RD, REF =  rd_rf_mem_di[-1].split()
+#    sys.stderr.write(str(len(rd_rf_mem_di)) + ' ')
+    last = None
+    rd_win_pos = ''
+    if i[-1] is None:
+    	last = i[0] + len(i) + 1
+        rd_win_pos = ':'.join(rd_pos[i[0]:last])
+    else:
+        last = i[-1] + 1
+	rd_win_pos = ':'.join(rd_pos[i[0]:last])
+    RD, REF = rd_rf_mem_di[-1].split()
     rd_kmer = ''.join (rd_bases[i[0]:last])
     rf_kmer = ''.join (rf_bases[i[0]:last])
+    int_ele = 0
     for k in (i):
-        print ",".join ([rd_kmer, rf_kmer, REF, rf_pos[k],rf_bases[k],
+	if isinstance (k, int):
+	        print >> slided_fh, ",".join ([relative_pos[int_ele],rd_win_pos,rd_kmer, rf_kmer, REF, rf_pos[k], rf_bases[k],
                                   RD,rd_pos[k],rd_bases[k],
                                   qual[k],mis[k],ins[k],delt[k]])
+		int_ele += 1
+	elif k is None:
+		print >> slided_fh, ",".join ([relative_pos[int_ele],rd_win_pos,rd_kmer, rf_kmer, REF, 'Nan,Nan',
+                                RD,'Nan,Nan,Nan,Nan,Nan,Nan'])
+		int_ele += 1
+
+slided_fh.close()
+### adjust deletion counts
+#### choose only centre lines
+'''
+-2,80:81:82:83:84,GTTTT,GTTTT+C+G,cc6m_2244_T7_ecorv,114,G,94e2685c-c9e3-42b5-9a36-2c9ed081c1bc,80,G,6,0,0,0
+-1,80:81:82:83:84,GTTTT,GTTTT+C+G,cc6m_2244_T7_ecorv,115,T,94e2685c-c9e3-42b5-9a36-2c9ed081c1bc,81,T,16,0,0,0
+0,80:81:82:83:84,GTTTT,GTTTT+C+G,cc6m_2244_T7_ecorv,116,T,94e2685c-c9e3-42b5-9a36-2c9ed081c1bc,82,T,21,0,0,0
++1,80:81:82:83:84,GTTTT,GTTTT+C+G,cc6m_2244_T7_ecorv,117,T,94e2685c-c9e3-42b5-9a36-2c9ed081c1bc,83,T,23,0,0,0
++2,80:81:82:83:84,GTTTT,GTTTT+C+G,cc6m_2244_T7_ecorv,118:119:120,T+C+G,94e2685c-c9e3-42b5-9a36-2c9ed081c1bc,84,T,17,0,0,2
+'''
+tmp_file = slided_file + '.tmp'
+tmp_out = open(tmp_file,'w')
+
+with open (slided_file,'r') as fh:
+	for l in fh:
+		if l.startswith('#'):
+			continue
+		ary =  l.strip().split (',')
+		if ary[0].startswith ('0'):
+			print >>tmp_out,l.strip()
+tmp_out.close()
+## allocate del to adjacent position
+deletions = {}
+err_file = tmp_file +'.err'
+err_fh = open (err_file,'w')
+with open (tmp_file,'r') as fh:
+	for l in fh:
+		if l.startswith('#'):
+			continue
+		ary = l.strip().split (',')
+		if len (ary) < 14:
+			continue
+		deletion = '' 
+		try:
+			deletion = int (ary[-1])
+		except:
+			print >> err_fh,l.strip() 
+			continue
+		if deletion > 0:
+			tmp = fh.next().strip().split(',')
+			new_del = int (round (deletion/2.0))
+			tmp[-1] = str(new_del)
+			ary[-1] = str(new_del)
+			k2 = ','.join (tmp[4:9])
+			k1 = ','.join (ary[4:9])
+			deletions[k1] =  ",".join(ary[4:])
+			deletions[k2] =  ",".join(tmp[4:])
+err_fh.close()
+fh.close()
+os.remove (tmp_file)
+
+adjusted_slide_file = slided_file + '.del.adjusted'
+out_fh = open (adjusted_slide_file,'w')
+with open (slided_file,'r') as fh:
+	for l in fh:
+		if l.startswith ('#'):
+			print >>out_fh, l.strip(); continue
+		ary = l.strip().split(',')
+		k = ','.join (ary[4:9])
+		if k in deletions:	
+#			sys.stderr.write(k+'\n')
+			print >>out_fh, ",".join (ary[0:4]) +','+ deletions[k]
+		else:
+			print >>out_fh, l.strip()
+out_fh.close()
+os.remove (slided_file)
+
+##########################################################################
+##############  sum up onebase oneline into onekmer one line format ######
+##########################################################################
+
+sum_file = adjusted_slide_file + '.summed.oneKmer_oneLine'
+sum_out = open (sum_file,'w')
+
+'''
+input
+#Relative_Pos,window,ReadKmer,RefKmer,Ref,RefPos,RefBase,Read,ReadPos,ReadBase,Q,M,I,D
+-2,2:3:4:5:6,TCGAT,TCGAT,cc6m_2244_T7_ecorv,46,T,94e2685c-c9e3-42b5-9a36-2c9ed081c1bc,2,T,5,0,0,0
+-1,2:3:4:5:6,TCGAT,TCGAT,cc6m_2244_T7_ecorv,47,C,94e2685c-c9e3-42b5-9a36-2c9ed081c1bc,3,C,6,0,0,0
+0,2:3:4:5:6,TCGAT,TCGAT,cc6m_2244_T7_ecorv,48,G,94e2685c-c9e3-42b5-9a36-2c9ed081c1bc,4,G,12,0,0,0
++1,2:3:4:5:6,TCGAT,TCGAT,cc6m_2244_T7_ecorv,49,A,94e2685c-c9e3-42b5-9a36-2c9ed081c1bc,5,A,10,0,0,0
++2,2:3:4:5:6,TCGAT,TCGAT,cc6m_2244_T7_ecorv,50,T,94e2685c-c9e3-42b5-9a36-2c9ed081c1bc,6,T,4,0,0,0
+'''
+from collections import defaultdict
+mem_window = defaultdict (defaultdict(list).copy)
+
+var = adjusted_slide_file
+if var.endswith ('.gz'):
+	f = gzip.open (var,'r')
+else:
+	f = open (var,'r')
+header = f.readline()
+new_header = ",".join (header.strip().split(',')[2:])
+print >>sum_out, "#Read_Window,ReadKmer,RefKmer,Ref,Read,Qualities,Mismatches,Insertions,Deletions"
+
+ary = f.readline().strip().split(',')
+k =  ','.join(ary[1:5]) + ','+ary[7]
+mem_window[k]['q'].append(ary[-4])
+mem_window[k]['m'].append(ary[-3])
+mem_window[k]['i'].append(ary[-2])
+mem_window[k]['d'].append(ary[-1])
+
+for l in  f:
+	ary = l.strip().split(',')
+	k =  ','.join(ary[1:5]) + ','+ary[7]
+	if k in mem_window:
+		mem_window[k]['q'].append(ary[-4])
+		mem_window[k]['m'].append(ary[-3])
+		mem_window[k]['i'].append(ary[-2])
+		mem_window[k]['d'].append(ary[-1])
+	else:
+		for kk in mem_window:
+			print >>sum_out, ','.join ([kk ,'|'.join (mem_window[kk]['q']),  '|'.join (mem_window[kk]['m']),  '|'.join (mem_window[kk]['i']),  '|'.join (mem_window[kk]['d'])])
+		del mem_window[kk]
+		mem_window[k]['q'].append(ary[-4])
+		mem_window[k]['m'].append(ary[-3])
+		mem_window[k]['i'].append(ary[-2])
+		mem_window[k]['d'].append(ary[-1])
+for kk in mem_window.keys():
+	print >>sum_out, ','.join ([kk ,'|'.join (mem_window[kk]['q']),  '|'.join (mem_window[kk]['m']),  '|'.join (mem_window[kk]['i']),  '|'.join (mem_window[kk]['d'])])		
+f.close()
+sum_out.close()
 
--- scripts/main/slide_per_site_var.py
+++ scripts/main/slide_per_site_var.py
@@ -1,6 +1,7 @@
 import sys
 import re
 from collections import OrderedDict
+from collections import defaultdict 
 
 mem = {}
 contents = OrderedDict()
@@ -19,8 +20,11 @@ with open (sys.argv[1],'r') as fh:
         ref, pos = ary[0], ary[1]
         contents[ref+':'+pos] = line.strip()
 
+out1 = sys.argv[1] +'.slided.onekmer.multiplelines.' + str(win_size) + 'mer.csv'
+outh1= open (out1,'w')
+
 header = '#Kmer,window,Relative_pos,Ref,Ref_Pos,base,cov,q_mean,q_median,q_std,mis,ins,del'
-print header
+print >>outh1, header
 for k in contents.keys():
     ref, pos = k.split(':')
     try:
@@ -58,4 +62,58 @@ for k in contents.keys():
             LINES.append ('+'+str(j)+','+'None')
     positions = ':'.join (POS)
     for l in LINES:
-        print upper+contents[k].split(',')[2] +down+',' +positions+','+ l
+        print >>outh1, upper+contents[k].split(',')[2] +down+',' +positions+','+ l
+outh1.close()
+############################################################################################
+###    sum up slided per site variants from multiple lines into single lines ###############
+############################################################################################
+'''
+input per site
+#Kmer,window,Relative_pos,Ref,Ref_Pos,base,cov,q_mean,q_median,q_std,mis,ins,del
+NNGTC,43:44:45:46:47,-2,Null
+NNGTC,43:44:45:46:47,-1,Null
+NNGTC,43:44:45:46:47,+0,cc6m_2244_T7_ecorv,45,G,1,5.000,5.000,0.000,0.000,0.000,0.000
+NNGTC,43:44:45:46:47,+1,cc6m_2244_T7_ecorv,46,T,3,7.333,8.000,1.700,0.000,0.000,0.000
+NNGTC,43:44:45:46:47,+2,cc6m_2244_T7_ecorv,47,C,3,5.333,6.000,1.700,0.000,0.000,0.000
+NGTCG,44:45:46:47:48,-2,Null
+NGTCG,44:45:46:47:48,-1,cc6m_2244_T7_ecorv,45,G,1,5.000,5.000,0.000,0.000,0.000,0.000
+NGTCG,44:45:46:47:48,+0,cc6m_2244_T7_ecorv,46,T,3,7.333,8.000,1.700,0.000,0.000,0.000
+NGTCG,44:45:46:47:48,+1,cc6m_2244_T7_ecorv,47,C,3,5.333,6.000,1.700,0.000,0.000,0.000
+NGTCG,44:45:46:47:48,+2,cc6m_2244_T7_ecorv,48,G,3,10.000,9.000,1.414,0.000,0.000,0.000
+GTCGA,45:46:47:48:49,-2,cc6m_2244_T7_ecorv,45,G,1,5.000,5.000,0.000,0.000,0.000,0.000
+'''
+
+mem_window = defaultdict (defaultdict(list).copy)
+k_pool = []
+f =  open (out1,'r')
+for l in f:
+    if l.startswith ('N'):
+        continue
+    elif l.startswith ('#'):
+	continue
+    ary = l.strip().split(',')
+    if ary[0].endswith ('N'):
+	continue
+    ks = ary[0:4]
+    del ks[2]
+    k = ",".join (ks)
+    k_pool.append (k)
+    #print l.strip()
+    mem_window[k]['q'].append(ary[7])
+    mem_window[k]['m'].append(ary[10])
+    mem_window[k]['i'].append(ary[11])
+    mem_window[k]['d'].append(ary[12])
+    mem_window[k]['cov'].append(ary[6])
+f.close()
+out2 = sys.argv[1] + '.slided.onekmer.oneline.'+str(win_size) +'mer.csv'
+outh2 = open (out2,'w')
+print >> outh2, '#Kmer,Window,Ref,Coverage,q1,q2,q3,q4,q5,mis1,mis2,mis3,mis4,mis5,ins1,ins2,ins3,ins4,ins5,del1,del2,del3,del4,del5'
+for k in k_pool:
+		Qs = ",".join (mem_window[k]['q'])
+		Mis = ",".join (mem_window[k]['m'])
+		Ins = ",".join (mem_window[k]['i'])
+		Del = ",".join (mem_window[k]['d'])
+		Cov =  ":".join (mem_window[k]['cov'])
+		print >> outh2, ",".join ([k,Cov,Qs,Mis,Ins,Del])
+outh2.close()
+
