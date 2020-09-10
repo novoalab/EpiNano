d53492de143c58540c70432e2ca25a5920988aa7
huanle <elzedliu@gmail.com>
2020-09-10 17:34:17 +0200

    add installation instructions

---
+++ INSTALL.sh
@@ -0,0 +1,11 @@
+#### this is a simple instruction to install required packages
+#### I am using python3.6
+
+python -m venv epinano1.2_venv
+
+source epinano1.2_venv/bin/activate
+
+pip install pandas==0.25.1
+
+pip install dask==2.5.2
+
