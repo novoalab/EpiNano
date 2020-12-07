af0a5486b9e683933526b2755cba8d91223d90be
kxk302 <kxk302@gmail.com>
2020-12-07 10:20:10 -0500

    Added missing dependencies to run Epinano_Variant.py

--- INSTALL.sh
+++ INSTALL.sh
@@ -6,6 +6,7 @@ python -m venv epinano1.2_venv
 source epinano1.2_venv/bin/activate
 
 pip install pandas==0.25.1
-
 pip install dask==2.5.2
-
+pip install toolz==0.11.1
+pip install fsspec==0.3.3
+pip install cloudpickle==1.6.0
